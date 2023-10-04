6ad486675c46481bc14d024d0cfb6b218bd67798
nullb3 <dasistnullbe@gmail.com>
2023-10-03 19:01:22 -0600

    Update infoControlador.java

--- demo/src/main/java/com/clase4/demo/infoControlador.java
+++ demo/src/main/java/com/clase4/demo/infoControlador.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //convertirlo en un rest controller.
-@RequestMapping("/estudiante")
+@RequestMapping("/info")
 public class infoControlador {
-    @GetMapping("/status")
+    @GetMapping
     public String status(){
-        return "online";
+        return "Online";
     }
 }
