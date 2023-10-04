a4863c9db984dd63c502180281e6da17ab574587
nullb3 <dasistnullbe@gmail.com>
2023-10-03 20:33:45 -0600

    Update PracticaControlador.java

--- demo/src/main/java/com/clase4/demo/PracticaControlador.java
+++ demo/src/main/java/com/clase4/demo/PracticaControlador.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/persona")
 public class PracticaControlador{
-    @GetMapping
+    @GetMapping("/saludar")
     public String Saludar(){
         return "Hola!";
     }
-    @GetMapping
+    @GetMapping("/greet")
     public String Greet(){
         return "Hello!";
     }
     @PostMapping("/administrar")
     public String PostName(String name){
-        System.out.println(name);
-        return "La persona dice que su nombre es "+name;
+        return "La persona POST dice que su nombre es "+name;
+    }
+    @GetMapping("/administrar")
+    public String GetName(String name){
+        return "La persona GET dice que su nombre es "+name;
     }
 }
